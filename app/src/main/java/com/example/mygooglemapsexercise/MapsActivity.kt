1b9d56df667acd45602c79f42e2821b82aabe9a5
Beratcan Güreş <guresberatcan@gmail.com>
2021-06-24 17:42:15 +0300

    Third Commit

--- app/src/main/java/com/example/mygooglemapsexercise/MapsActivity.kt
+++ app/src/main/java/com/example/mygooglemapsexercise/MapsActivity.kt
@@ -1,20 +1,29 @@
 package com.example.mygooglemapsexercise
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
-
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mygooglemapsexercise.databinding.ActivityMapsBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.example.mygooglemapsexercise.databinding.ActivityMapsBinding
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var buttonSend: Button
+    private lateinit var editTextLatitude: EditText
+    private lateinit var editTextLongitude: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +31,35 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        buttonSend = findViewById(R.id.buttonSend)
+        editTextLatitude = findViewById(R.id.editTextLatitude)
+        editTextLongitude = findViewById(R.id.editTextLongitude)
+
+        val height = 100
+        val width = 100
+        val b = BitmapFactory.decodeResource(resources, R.drawable.rocketicon)
+        val smallMarker = Bitmap.createScaledBitmap(b, width, height, false)
+        val smallMarkerIcon = BitmapDescriptorFactory.fromBitmap(smallMarker)
+
+        buttonSend.setOnClickListener {
+            val latitude = editTextLatitude.text.toString()
+            val longitude = editTextLongitude.text.toString()
+            val location = LatLng(latitude.toDouble(), longitude.toDouble())
+            mMap.addMarker(
+                MarkerOptions().position(location).title("You are here.").icon(BitmapDescriptorFactory.fromResource(R.drawable.smallMarkerIcon))
+            )
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
+            mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+        }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
